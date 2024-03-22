c4385954c44a9d65737ff5cfa6e2ad8f75d23156
trakt <trakt@DESKTOP-F1DCND3>
2024-03-22 17:32:55 +0200

    Добавьте файлы проекта.

---
+++ vector/vector.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+class Point
+{
+	
+public:
+	int y;
+	Point(int x) : y(x) {}
+
+};
+
+int main()
+{
+	vector<Point> obj1 { 1,2,3,4,5 };
+	
+	for (auto i : obj1)
+	{
+		cout << "Vector: " <<  i.y << endl;
+	}
+
+
+	list<Point> obj2;
+
+	for (int i = 1; i <= 5; ++i) 
+	{
+		Point p(i);
+		obj2.push_back(p);
+	}
+
+	for (const auto& point : obj2) 
+	{
+		cout << "List: " << point.y << endl;
+	}
+
+
+	
+
+}
+
